db2f8e6a0dd0e17166f5e2bdcee79bb9dda29e3f
Nick Korotysh <kolchaprogrammer@list.ru>
2022-06-03 01:09:51 +0200

    some uncommitted changes found locally

--- application.cpp
+++ application.cpp
@@ -28,8 +28,11 @@ bool Application::event(QEvent* e)
 {
   if (e->type() == QEvent::FileOpen) {
     QFileOpenEvent *open_event = static_cast<QFileOpenEvent*>(e);
-    emit fileOpened(open_event->file());
-    open_event->accept();
+    QString file = !open_event->file().isEmpty() ? open_event->file() : open_event->url().toLocalFile();
+    if (!file.isEmpty()) {
+      emit fileOpened(open_event->file());
+      open_event->accept();
+    }
     return open_event->isAccepted();
   }
   return QApplication::event(e);

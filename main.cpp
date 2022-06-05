2cbf9c652ad73b08141709345335255ee683bfcf
Nick Korotysh <kolchaprogrammer@list.ru>
2022-06-05 10:41:33 +0200

    version update

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 {
   QApplication::setApplicationDisplayName("SeeX");
   QApplication::setApplicationName("SeeX");
-  QApplication::setApplicationVersion("1.0.5.130");
+  QApplication::setApplicationVersion("1.1.0");
   QApplication::setOrganizationName("Nick Korotysh");
   QApplication::setOrganizationDomain("seex.kolcha.github.io");
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
